f026477f50c68173770e3be33c8d1c287d7b2669
BattleHeart <dwlsxj@126.com>
2020-11-22 22:55:37 +0800

    feat:添加了leetcode05暴力解法

--- src/main/java/cn/battleheart/leetcode/LeetCode05.java
+++ src/main/java/cn/battleheart/leetcode/LeetCode05.java
@@ -1,5 +1,11 @@
 package cn.battleheart.leetcode;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 5. 最长回文子串
  * 含义:正向和反向是一样的字符串。
@@ -18,18 +24,51 @@ package cn.battleheart.leetcode;
  */
 public class LeetCode05 {
     public static void main(String[] args) {
+        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("asddsaasdasdasdasd"));
+        System.out.println(longestPalindrome("babad"));
     }
 
-    public String longestPalindrome(String s) {
+    /**
+     * 方法一：暴力破解方法。
+     * 原理就是所有字符串都比较一遍是否是回文子串，从两个字符到结尾
+     * 回文子串的含义：a[i]=a[length-1]的数字，如果不是则不是回文。直接退出就行。
+     *
+     * @param s 字符串。
+     * @return 返回最大回文子串。
+     */
+    public static String longestPalindrome(String s) {
         int length = s.length();
-
-        // 滑动窗口的方式。
+        int maxLength = 0;
+        int startIndex = 0;
+        int j = 0;
         for (int i = 0; i < s.length(); i++) {
-
-            while (i + 1 < length) {
-
+            for (int k = i + 1; k < s.length(); k++) {
+                if (k - i + 1 > maxLength && validate(s, i, k)) {
+                    maxLength = k - i + 1;
+                    startIndex = i;
+                }
             }
         }
-        return "";
+        return s.substring(startIndex, startIndex + maxLength);
+    }
+
+    /**
+     * 验证是不是回文子串的方法。
+     *
+     * @param s     字符串。
+     * @param start 开始位置。
+     * @param end   结束为止。
+     * @return 返回true=是，返回false=不是。
+     */
+    private static boolean validate(String s, int start, int end) {
+        while (end > start) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            end--;
+            start++;
+        }
+        return true;
     }
 }
