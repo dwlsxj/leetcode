a4c0afb157b210d49e5ecb3f1dc50c880b40b644
BattleHeart <dwlsxj@126.com>
2021-05-01 14:56:13 +0800

    feat:添加了KMP的next函数，以及KMP匹配算法

--- src/main/java/cn/battleheart/play/string/StringFind.java
+++ src/main/java/cn/battleheart/play/string/StringFind.java
@@ -15,22 +15,23 @@ public class StringFind {
      * g o o d g o o g l e
      * g o o g l e
      * 🔼
-     *   🔽
+     * 🔽
      * g o o d g o o g l e
      * g o o g l e
-     *   🔼
-     *     🔽
+     * 🔼
+     * 🔽
      * g o o d g o o g l e
      * g o o g l e
-     *     🔼
-     *       🔽
+     * 🔼
+     * 🔽
      * g o o d g o o g l e
      * g o o g l e
-     *       🔼
-     *   🔽
+     * 🔼
+     * 🔽
      * g o o d g o o g l e
-     *   g o o g l e
-     *   🔼
+     * g o o g l e
+     * 🔼
+     *
      * @param s 主串。
      * @param t
      * @return
@@ -66,12 +67,69 @@ public class StringFind {
         }
     }
 
+    /**
+     * kmp中获取next值。
+     * 规则如下：
+     * 1. 当模式串中的p[j]=p[k]时，next[j]=k+1;
+     * 2. 当模式串中的p[j]≠p[k]时，需要回溯到上次相同的位置，也就是k=next[k]的位置。
+     *
+     * @param text 需要转换为next值的字符串。
+     * @return 返回next的集合信息。
+     */
+    public static int[] getNext(String text) {
+        int[] next = new int[text.length()];
+        int j = 0;
+        int k = -1;
+        next[0] = k;
+        while (j < text.length() - 1) {
+            if (k == -1 || text.charAt(j) == text.charAt(k)) {
+                ++j;
+                ++k;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
 
+    /**
+     * 匹配子串在主串的首位置。
+     *
+     * @param s         主字符串。
+     * @param subString 子串。
+     * @return 返回位置。
+     */
+    public static int getKmpIndexString(String s, String subString) {
+        int i = 0;
+        int j = 0;
+        int[] next = getNext(subString);
+
+        while (i < s.length() && j < subString.length()) {
+            if (j == -1 || s.charAt(i) == subString.charAt(j)) {
+                ++i;
+                ++j;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j > subString.length() - 1) {
+            return i - subString.length();
+        } else {
+            return -1;
+        }
+    }
 
 
     public static void main(String[] args) {
-        String s = "goodgoogle";
-        String t = "google";
-        System.out.println(getFirstIndex(s, t));
+        int[] ababaaababas = getNext("ababaaababaa");
+        for (int i = 0; i < ababaaababas.length; i++) {
+            System.out.print(ababaaababas[i] + " ");
+        }
+
+        System.out.println();
+        int index = getKmpIndexString("ababaaababaa", "aaab");
+        System.out.println(index);
+
     }
 }
